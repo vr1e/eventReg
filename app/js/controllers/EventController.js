de20b3ebb76a0c2ad12362645db5d7ec200d139e
Nikola Vrhovac <vrllee@gmail.com>
2017-07-11 21:33:57 +0200

    Add ng-repeat

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -12,7 +12,30 @@ eventsApp.controller('EventController',
                 city: 'Mountain View',
                 province: 'CA'
             },
-            imageUrl: '/img/angularjs-logo.png'
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this sessions you will learn the ins and outs of directives!'
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: '30 mins',
+                    level: 'Introductory',
+                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.'
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.'
+                }
+            ]
         }
 
     }
