810703e3bd6120cdca88363b8a7e168b7ec12d66
Nikola Vrhovac <vrllee@gmail.com>
2017-07-11 21:47:09 +0200

    Add on click event counters

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -19,23 +19,34 @@ eventsApp.controller('EventController',
                     creatorName: 'Bob Smith',
                     duration: '1 hr',
                     level: 'Advanced',
-                    abstract: 'In this sessions you will learn the ins and outs of directives!'
+                    abstract: 'In this sessions you will learn the ins and outs of directives!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
                     duration: '30 mins',
                     level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.'
+                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
                     duration: '2 hours',
                     level: 'Intermediate',
-                    abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.'
+                    abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
+                    upVoteCount: 0
                 }
             ]
+        }
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
         }
 
     }
