5df0f29d8bced3fd9f90970a07403962b75ae48e
Nikola Vrhovac <vrllee@gmail.com>
2017-07-14 17:43:03 +0200

    Add custom filters

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -7,9 +7,11 @@ eventsApp.controller('EventController',
         $scope.boolValue = false;
         $scope.mystyle = {color: 'red'};
         $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
+        $scope.sortorder = 'name';
         $scope.event = {
             name: 'Angular Boot Camp',
-            date: '1/1/2013',
+            date: 1359781015626,
             time: '10:30 am',
             location: {
                 address: 'Google Headquaters',
@@ -21,7 +23,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1 hr',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this sessions you will learn the ins and outs of directives!',
                     upVoteCount: 0
@@ -29,7 +31,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '30 mins',
+                    duration: 2,
                     level: 'Introductory',
                     abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
                     upVoteCount: 0
@@ -37,7 +39,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
-                    duration: '2 hours',
+                    duration: 4,
                     level: 'Intermediate',
                     abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
                     upVoteCount: 0
