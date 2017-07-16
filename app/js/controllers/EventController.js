02ed539fd447261f5a23b079386fc3b5ac4395ce
Nikola Vrhovac <vrllee@gmail.com>
2017-07-16 22:56:38 +0200

    Add data service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,13 +3,15 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData) {
 
-        $scope.snippet = '<span style="color: red">hi there</span>';
-        $scope.boolValue = false;
-        $scope.mystyle = {color: 'red'};
-        $scope.myclass = "blue";
+        // $scope.snippet = '<span style="color: red">hi there</span>';
+        // $scope.boolValue = false;
+        // $scope.mystyle = {color: 'red'};
+        // $scope.myclass = "blue";
         $scope.buttonDisabled = true;
         $scope.sortorder = 'name';
-        $scope.event = eventData.event;
+        eventData.getEvent(function(event) {
+            $scope.event = event;
+        });
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
