36b809c67d285a45e47dd50b2a22c5efec92c778
Nikola Vrhovac <vrllee@gmail.com>
2017-07-20 12:56:10 +0200

    Use  promises

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,21 +1,15 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData) {
+    function EventController($scope, eventData, $log) {
 
-        // $scope.snippet = '<span style="color: red">hi there</span>';
-        // $scope.boolValue = false;
-        // $scope.mystyle = {color: 'red'};
-        // $scope.myclass = "blue";
         $scope.buttonDisabled = true;
         $scope.sortorder = 'name';
-        $scope.event = eventData.getEvent(function(event) {
-            $scope.event = event;
-        });
-
-        // eventData.getEvent(function(event) {
-        //     $scope.event = event;
-        // });
+        eventData.getEvent()
+            .success(function(event) { $scope.event = event; })
+            .error(function (data, status, headers, config) {
+                $log.warn(data, status, headers(), config);
+            });
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
