8c309d07a7ae076161681f9c5b7a0309decf336a
Nikola Vrhovac <nikola.vrhovac@gowi.rs>
2017-12-07 18:43:00 +0100

    exceptionHandler service

--- app/js/controllers/LocaleSampleController.js
+++ app/js/controllers/LocaleSampleController.js
@@ -6,6 +6,8 @@ eventsApp.controller('LocaleSampleController',
     console.log($locale);
 		$scope.myDate = Date.now();
 		$scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+    
+    throw { message: 'error message' };
 
 	}
 );
