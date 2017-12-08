d52a01c4f50b64b4d89f7e2ef099c77a2840d3b0
Nikola Vrhovac <nikola.vrhovac@gowi.rs>
2017-12-08 18:17:13 +0100

    default route

--- app/js/app.js
+++ app/js/app.js
@@ -14,4 +14,5 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
 			templateUrl: 'templates/EventDetails.html',
 			controller: 'EventController'
 		});
+    $routeProvider.otherwise({redirectTo: '/events'});
 	});
