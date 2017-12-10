2691582bc3d0d7dd816906de4017546a83f14684
Nikola Vrhovac <vrllee@gmail.com>
2017-12-10 15:54:37 +0100

    angular directive

---
+++ app/js/directives.js
@@ -0,0 +1,8 @@
+'use strict';
+
+eventsApp.directive('mySample', function() {
+  return {
+    restrict: 'C',
+    template: "<input type='text' ng-model='sampleData' /> {{sampleData}}<br />"
+  };
+});
