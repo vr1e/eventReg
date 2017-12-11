2ba874cb742392de99b0fad7fb1b922db63d68b1
Nikola Vrhovac <vrllee@gmail.com>
2017-12-11 13:59:26 +0100

    using require to share directive controllers

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -1,18 +1,49 @@
 'use strict';
 
-eventsApp.directive('greeting', function() {
-  return {
-    restrict: 'E',
-    replace: true,
-    template: '<button class="btn" ng-click="sayHello()">Say Hello</button>',
-    controller: '@',
-    name: 'ctrl'
-  }
-});
-
-eventsApp.controller('GreetingController',
-  function GreetingController($scope) {
-    $scope.sayHello = function() {
-      alert('Hello');
+eventsApp
+  .directive('greeting', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      template: '<button class="btn" ng-click="sayHello()">Say Hello</button>',
+      controller: function ($scope) {
+        var greetings = ['hello'];
+        $scope.sayHello = function() {
+          alert(greetings.join());
+        }
+        this.addGreeting = function(greeting) {
+          greetings = [];
+          greetings.push(greeting);
+        }
+      }
     }
-});
+  })
+  .directive('finnish', function() {
+    return {
+      restrict: 'A',
+      require: 'greeting',
+      link: function(scope, element, attrs, controller) {
+        controller.addGreeting('hei');
+      }
+    }
+  })
+  .directive('hindi', function() {
+    return {
+      restrict: 'A',
+      require: 'greeting',
+      link: function(scope, element, attrs, controller) {
+        controller.addGreeting('नमस्ते');
+      }
+    }
+  });
+
+// eventsApp.controller('GreetingController',
+//   function GreetingController($scope) {
+//     var greetings = ['hello'];
+//     $scope.sayHello = function() {
+//       alert(greetings.join());
+//     }
+//     this.addGreeting = function(greeting) {
+//       greetnigs.push(greeting);
+//     }
+// });
